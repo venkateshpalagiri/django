1ea6176268496e4b562f656ab9b505901172c2fa
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-12-11 11:59:16 -0500

    END points added

--- demo_proj/api_app/models.py
+++ demo_proj/api_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name=models.CharField(max_length=50)
+    age=models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    name=models.CharField(max_length=50)
+    city=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name +" "+ self.city
--- demo_proj/api_app/views.py
+++ demo_proj/api_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User,Employee
+from .serializer import UserSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_users(request):
+    return Response(UserSerializer({'name':'venky','city':'Tor'}))
+
+@api_view(['POST'])
+def create_user(request):
+    serializer=UserSerializer(data=request)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
